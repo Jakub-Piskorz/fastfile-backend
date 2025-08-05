8d0fb8a8df3a9ceca24c91ea5a818ca26593d7c1
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-08-05 11:34:50 +0200

    fix: cors config for corporate proxies

--- src/main/java/com/fastfile/config/CorsConfig.java
+++ src/main/java/com/fastfile/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package com.fastfile.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,9 +14,24 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all endpoints
-                .allowedOrigins("*")  // Allow all origins
+                .allowedOriginPatterns("*")  // Allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow all HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .exposedHeaders("Content-Disposition", "Pragma", "Cache-Control", "Expires", "Content-Type");  // Expose download file headers
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
