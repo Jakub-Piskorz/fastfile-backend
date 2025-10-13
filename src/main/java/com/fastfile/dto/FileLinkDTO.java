889cf698e0f1f25a14cfcc6dce48a43d990a5f57
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-10-13 23:34:13 +0200

    refactor: file links

--- src/main/java/com/fastfile/dto/FileLinkDTO.java
+++ src/main/java/com/fastfile/dto/FileLinkDTO.java
@@ -2,5 +2,5 @@ package com.fastfile.dto;
 
 import com.fastfile.model.FileLink;
 
-public record FileLinkDTO(FileMetadataDTO fileMetadataDTO, FileLink fileLink) {
+public record FileLinkDTO(FileMetadataDTO metadata, FileLink fileLink) {
 }
