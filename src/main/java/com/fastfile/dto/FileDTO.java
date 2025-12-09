93620dd74891886df713b975b1ab7793ac9dda63
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2025-12-10 00:46:18 +0100

    chore: required metadata

--- src/main/java/com/fastfile/dto/FileDTO.java
+++ src/main/java/com/fastfile/dto/FileDTO.java
@@ -2,5 +2,6 @@ package com.fastfile.dto;
 
 import com.fastfile.model.FileLink;
 import com.fastfile.model.FileMetadata;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record FileDTO(FileMetadata metadata, FileLink fileLink) {}
+public record FileDTO(@Schema(requiredMode = Schema.RequiredMode.REQUIRED) FileMetadata metadata, FileLink fileLink) {}
